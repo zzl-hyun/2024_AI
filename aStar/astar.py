f191087204d999fd7b6d68fd27b16fa584c366a4
kimuki <kgh9985@naver.com>
2024-05-10 21:48:23 +0900

    realrealfinal

--- aStar/astar.py
+++ aStar/astar.py
@@ -20,7 +20,7 @@ VISIT = (175, 238, 238)
 CELL = 30  
 
 # 방문노드 그리는 함수
-def liveDrawVisit(screen, maze, start, end, visit):
+def liveDrawVisit(screen, start, visit):
     
     for row, col in visit:
         color = VISIT
@@ -28,31 +28,31 @@ def liveDrawVisit(screen, maze, start, end, visit):
         if (row, col) == start:  
             font = pygame.font.Font(None, 18)
             text = font.render("S", True, BLACK) 
-            text_rect = text.get_rect(center=((col + 0.5) * CELL, (row + 0.5) * CELL))
-            screen.blit(text, text_rect)
+            textRect = text.get_rect(center=((col + 0.5) * CELL, (row + 0.5) * CELL))
+            screen.blit(text, textRect)
 
-def liveDrawOpen(screen, maze, start, end, visit):
+def liveDrawOpen(screen, start, visit):
     for row, col in visit:
         color = OPEN
         pygame.draw.rect(screen, color, (col * CELL + 1, row * CELL + 1, CELL - 2, CELL - 2))
         if (row, col) == start:  
             font = pygame.font.Font(None, 18)
             text = font.render("S", True, BLACK) 
-            text_rect = text.get_rect(center=((col + 0.5) * CELL, (row + 0.5) * CELL))
-            screen.blit(text, text_rect)
+            textRect = text.get_rect(center=((col + 0.5) * CELL, (row + 0.5) * CELL))
+            screen.blit(text, textRect)
    
     pygame.display.flip()
 
 class Node:
-    def __init__(self, parent=None, position=None):
+    def __init__(self, parent=None, pos=None):
         self.parent = parent
-        self.position = position
+        self.pos = pos
         self.g = 0
         self.h = 0
         self.f = 0
 
     def __eq__(self, other):
-        return self.position == other.position
+        return self.pos == other.pos
 
     def __lt__(self, other):
         # tie breaking 적용
@@ -61,150 +61,148 @@ class Node:
         return self.f < other.f
 
 def Manhattan(node, goal):  # 맨해튼 거리
-    dx = abs(node.position[0] - goal.position[0])
-    dy = abs(node.position[1] - goal.position[1])
+    dx = abs(node.pos[0] - goal.pos[0])
+    dy = abs(node.pos[1] - goal.pos[1])
     return (dx + dy)
 
 def Euclidean(node, goal):  # 유클리드 거리
-    dx = abs(node.position[0] - goal.position[0])
-    dy = abs(node.position[1] - goal.position[1])
+    dx = abs(node.pos[0] - goal.pos[0])
+    dy = abs(node.pos[1] - goal.pos[1])
     return (dx * dx + dy * dy) ** 0.5
 
-def aStar(maze, start, end, heuristic, screen):
+def aStar(grid, start, end, heuristic, screen):
     # 초기화
+    M = len(grid) - 1 
+    N = len(grid[0]) - 1 
+    move = [(0, 1), (0, -1), (1, 0), (-1, 0)] 
+    #move = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, 1), (-1, -1), (1, 1), (1, -1)]
     startNode = Node(None, start)
     endNode = Node(None, end)
-
     openList = PriorityQueue()
     openSet = set()
-    closedSet = set()
-
+    closeSet = set()
+    minFnode = Node(startNode, start)
+    minFnode.f = 99999999999
     explored = 0
-    min_f_node = Node(startNode, start)
-    min_f_node.f = 99999999999
 
     openList.put((startNode.f, startNode))
-    openSet.add(startNode.position)
+    openSet.add(startNode.pos)
 
     while not openList.empty():
         # 현재 노드 지정
-        currentNode = openList.get()[1]
-        openSet.remove(currentNode.position)
-        closedSet.add(currentNode.position)
-        liveDrawVisit(screen, maze, start, end, closedSet)
+        curNode = openList.get()[1]
+        openSet.remove(curNode.pos)
+        closeSet.add(curNode.pos)
+        liveDrawVisit(screen, start, closeSet)
 
-        # 현재 노드가 목적지면 current.position 추가하고
-        # current의 부모로 이동
-        if currentNode == endNode:
+        # 목적지를 찾으면 경로 반환
+        if curNode == endNode:
             path = []
             print("search!!")
+            while curNode is not None:
+                path.append(curNode.pos)
+                curNode = curNode.parent
+            for _ in closeSet:
+                explored += 1
             print("total explored nodes: ", explored)
-            while currentNode is not None:
-                path.append(currentNode.position)
-                currentNode = currentNode.parent
-            return path[::-1], closedSet
+            return path, closeSet
 
-        # 인접한 xy좌표 전부, DFS
-        for newPosition in [(0, -1), (0, 1), (-1, 0), (1, 0)] :#, (-1, -1), (-1, 1), (1, -1), (1, 1):
-            # 노드 위치 업데이트
-            nodePosition = (currentNode.position[0] + newPosition[0], currentNode.position[1] + newPosition[1])
-
-             # 미로 maze index 범위 안에 있어야함
-            if nodePosition[0] > len(maze) - 1 or nodePosition[0] < 0 or nodePosition[1] > len(maze[0]) - 1 or nodePosition[1] < 0:
+        # 대각선 비허용
+        for newPos in move:
+            curPos = (curNode.pos[0] + newPos[0], curNode.pos[1] + newPos[1])
+             #grid 범위 만족
+            if curPos[0] > M or curPos[0] < 0 or curPos[1] > N or curPos[1] < 0:
                 continue
-
-            # 장애물이 있으면 다른 위치 불러오기
-            if maze[nodePosition[0]][nodePosition[1]] != 0:
+            # 장애물이면 넘어가기
+            if grid[curPos[0]][curPos[1]] != 0:
                 continue
-
-            new_node = Node(currentNode, nodePosition)
-            # 자식이 closedList에 있으면 continue
-
-            if new_node.position in closedSet:
+            newNode = Node(curNode, curPos)
+            # 이미 방문한 노드면 넘어가기
+            if newNode.pos in closeSet:
                 continue
-            
             # f, g, h값 업데이트
-            new_node.g = currentNode.g + 1
             if heuristic == 1 or heuristic == None:
-                new_node.h = Manhattan(new_node, endNode)
+                newNode.h = Manhattan(newNode, endNode)
+                newNode.g = curNode.g + 1
             else:
-                new_node.h = Euclidean(new_node, endNode)
-            new_node.f = new_node.g + new_node.h
+                newNode.h = Euclidean(newNode, endNode)
+                #g가 f에 영향을 많이 끼쳐서 0.5로 줄임
+                newNode.g = curNode.g + 0.5
+            newNode.f = newNode.g + newNode.h
            
-
-            if new_node.position in openSet:
+            if newNode.pos in openSet:
                 for node in openList.queue:
-                    if node[1] == new_node and node[1].g > new_node.g:
-                        node[1].g = new_node.g
-                        node[1].f = new_node.f
-                        node[1].parent = new_node.parent
+                    if node[1] == newNode and node[1].g > newNode.g:
+                        node[1].g = newNode.g
+                        node[1].f = newNode.f
+                        node[1].parent = newNode.parent
                         break
             else:
-                openList.put((new_node.f, new_node))
-                openSet.add(new_node.position)
-                #print(new_node.position, new_node.g, new_node.h, new_node.f)
-                liveDrawOpen(screen, maze, start, end, openSet)
+                openList.put((newNode.f, newNode))
+                openSet.add(newNode.pos)
+                print(newNode.pos, newNode.g, newNode.h, newNode.f)
+                liveDrawOpen(screen, start, openSet)
 
-            if new_node.f <= min_f_node.f :
-                min_f_node = new_node
-                #print("min 갱신" + str(min_f_node.position))
-            explored += 1
-
+        if curNode.f != 0 and curNode.f <= minFnode.f and curNode.g >= minFnode.g:
+            minFnode = curNode
+            print("min 갱신" ,minFnode.pos, minFnode.f)
+            
     # f(n) 값이 가장 낮은 노드부터 시작점까지의 경로 출력
     print("경로를 찾을 수 없습니다.")
     path = []
-    current_node = min_f_node
-    while current_node != startNode:
-        path.append(current_node.position)
-        current_node = current_node.parent
+    curNode = minFnode
+    while curNode != startNode:
+        path.append(curNode.pos)
+        curNode = curNode.parent
 
-    path.append(current_node.position)
+    path.append(curNode.pos)
     #print(path)
+    for _ in closeSet:
+        explored += 1
     print("total explored nodes: ", explored)
-    return path, closedSet
+    return path, closeSet
 
 def initPoint(m, n) :
-    # 디버깅
-    #start = (1, 1)
-    #end = (m - 1, n - 1)
+    # 고정
+    start = (0, 0)
+    end = (m - 1, n - 1)
     # 랜덤 생성
-    start = (random.randint(0, m - 1), random.randint(0, n - 1))
-    end = (random.randint(0, m - 1), random.randint(0, n - 1))
+    #start = (random.randint(0, m - 1), random.randint(0, n - 1))
+    #end = (random.randint(0, m - 1), random.randint(0, n - 1))
     path = [None]
     visit = [None]
-    print(f"Start: {start}")
-    print(f"End: {end}")
+    print("Start:", start)
+    print("End: ", end)
     return start, end, path, visit
 
-def generateMaze(m, n, start, end, inc_obstacle_ratio):
+def generateGrid(m, n, start, end, inc_obstacle_ratio):
     # 모든 셀을 0으로 초기화
-    maze = [[0 for _ in range(n)] for _ in range(m)]
+    grid = [[0 for _ in range(n)] for _ in range(m)]
 
     # ratio에 맞게 장애물 계산
-    num_obstacles = int(m * n * inc_obstacle_ratio)
-    #print(num_obstacles)
+    obstaclesNum = int(m * n * inc_obstacle_ratio)
+    #print(obstaclesNum)
 
     # 장애물 랜덤 배치
-    for _ in range(num_obstacles):
+    for _ in range(obstaclesNum):
         while True:
             row = random.randint(0, m - 1)
             col = random.randint(0, n - 1)
 
             # 시작점과 종료점은 장애물 예외
-            if (row, col) != start and (row, col) != end and maze[row][col] == 0:
-                maze[row][col] = 1
+            if (row, col) != start and (row, col) != end and grid[row][col] == 0:
+                grid[row][col] = 1
                 break
-    return maze
+    return grid
 
-def draw_maze(screen, maze, start, end, path, visit):
+def drawGrid(screen, grid, start, end, path, visit):
 
-    for row in range(len(maze)):
-        for col in range(len(maze[0])):
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
             # 격자
             pygame.draw.rect(screen, BLACK, (col * CELL, row * CELL, CELL, CELL), 1)
-
             # 벽            
-            if maze[row][col] == 1:
+            if grid[row][col] == 1:
                 color = WALL 
             # 시작점
             elif (row, col) == start:
@@ -224,29 +222,29 @@ def draw_maze(screen, maze, start, end, path, visit):
             if (row, col) == start:  
                     font = pygame.font.Font(None, 18)
                     text = font.render("S", True, BLACK) 
-                    text_rect = text.get_rect(center=((col + 0.5) * CELL, (row + 0.5) * CELL))
-                    screen.blit(text, text_rect)
+                    textRect = text.get_rect(center=((col + 0.5) * CELL, (row + 0.5) * CELL))
+                    screen.blit(text, textRect)
 
             if (row, col) == end:  
                 font = pygame.font.Font(None, 18)
                 text = font.render("G", True, BLACK)  
-                text_rect = text.get_rect(center=((col + 0.5) * CELL, (row + 0.5) * CELL))
-                screen.blit(text, text_rect)
+                textRect = text.get_rect(center=((col + 0.5) * CELL, (row + 0.5) * CELL))
+                screen.blit(text, textRect)
             
             # 경로 생성
             # 좌표가 최소 2개 있을 때
             if len(path) >= 2:  
                 pygame.draw.lines(screen, YELLOW, False, [(p[1] * CELL + CELL/2, p[0] * CELL + CELL/2) for p in path], 5)
 
-def initDraw(screen, maze, start, end):
+def initDraw(screen, grid, start, end):
 
-    for row in range(len(maze)):
-        for col in range(len(maze[0])):
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
             # 격자 생성
             pygame.draw.rect(screen, BLACK, (col * CELL, row * CELL, CELL, CELL), 1)
 
             # 벽 색
-            if maze[row][col] == 1:
+            if grid[row][col] == 1:
                 color = WALL
             # 길 색  
             else:
@@ -257,17 +255,17 @@ def initDraw(screen, maze, start, end):
             if (row, col) == start:  
                     font = pygame.font.Font(None, 18)
                     text = font.render("S", True, BLACK)  
-                    text_rect = text.get_rect(center=((col + 0.5) * CELL, (row + 0.5) * CELL))
-                    screen.blit(text, text_rect)
+                    textRect = text.get_rect(center=((col + 0.5) * CELL, (row + 0.5) * CELL))
+                    screen.blit(text, textRect)
             # 목적지
             if (row, col) == end:  
                 font = pygame.font.Font(None, 18)
                 text = font.render("G", True, BLACK)  
-                text_rect = text.get_rect(center=((col + 0.5) * CELL, (row + 0.5) * CELL))
-                screen.blit(text, text_rect)
+                textRect = text.get_rect(center=((col + 0.5) * CELL, (row + 0.5) * CELL))
+                screen.blit(text, textRect)
 
 def main(m, n, r):
-    print(m, "x", n, "\nratio: ", r)
+    print(f"grid: {m} x {n} \nratio: {r}")
     WIDTH = n * CELL + 150 
     HEIGHT = m * CELL + 120
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -277,11 +275,11 @@ def main(m, n, r):
     start, end, path, visit = initPoint(m, n)
 
     # 미로 생성
-    maze = generateMaze(m, n, start, end, r)
+    grid = generateGrid(m, n, start, end, r)
 
     # 콘솔 출력
     '''
-    for row in maze:
+    for row in grid:
         print(' '.join(map(str, row)))
     '''
 
@@ -318,9 +316,7 @@ def main(m, n, r):
 
  
     dragging = False
-    selectedCell = None
-    
-
+    focusCell = None
     running = True
     while running:
         for event in pygame.event.get():
@@ -331,25 +327,24 @@ def main(m, n, r):
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:  # 좌클릭
                         pos = pygame.mouse.get_pos()
-
                         col = pos[0] // CELL
                         row = pos[1] // CELL
                         # 클릭한 좌표
-                        #print(col , "," , row)
+                        #print(f"{col} , {row})
 
-                        # maze 범위 안에 있어야 함
+                        # grid 범위 안에 있어야 함
                         if row < 0 or row >= m or col < 0 or col >= n:
                             pass
                         # 드래그
                         elif (row,col) == start or (row,col) == end:
                             dragging = True
-                            selectedCell = (row, col)
+                            focusCell = (row, col)
                         # 벽으로 설정
-                        elif maze[row][col] == 0:  
-                            maze[row][col] = 1
+                        elif grid[row][col] == 0:  
+                            grid[row][col] = 1
                         # 길로 설정
-                        elif maze[row][col] == 1:  
-                            maze[row][col] = 0
+                        elif grid[row][col] == 1:  
+                            grid[row][col] = 0
 
                         #start
                         if startButton.collidepoint(pos):
@@ -359,54 +354,54 @@ def main(m, n, r):
                             print("\nStart\n")
                             path = [None]
                             visit = [None]
-                            initDraw(screen, maze, start, end)
+                            initDraw(screen, grid, start, end)
                             # A* 알고리즘 실행
-                            path, visit = aStar(maze, start, end, radioButtons.value, screen)  
+                            path, visit = aStar(grid, start, end, radioButtons.value, screen)  
                         #random walls
                         if randomButton.collidepoint(pos):
                             start, end, path, visit = initPoint(m, n)
                             app.init(container)
-                            maze = generateMaze(m, n, start, end, r)  
+                            grid = generateGrid(m, n, start, end, r)  
                         #reset
                         if resetButton.collidepoint(pos):
-                            initDraw(screen, maze, start, end)
+                            initDraw(screen, grid, start, end)
                             path = [None]
                             visit = [None]
-                            screen.blit(text, text_rect)
+                            screen.blit(text, textRect)
                             pygame.display.flip()
                 elif event.type == pygame.MOUSEBUTTONUP:
                     if event.button == 1:
                         # 마우스 왼쪽 버튼을 놓으면 드래그 종료
                         dragging = False
-                        if selectedCell:
-                            if selectedCell == start:
+                        if focusCell:
+                            if focusCell == start:
                                 start = (row, col)
-                            elif selectedCell == end:
+                            elif focusCell == end:
                                 end = (row, col)
-                        selectedCell = None
+                        focusCell = None
                 elif event.type == pygame.MOUSEMOTION:
                     # 마우스를 이동하면 선택된 셀을 이동
-                    if dragging and selectedCell:
+                    if dragging and focusCell:
                         pos = pygame.mouse.get_pos()
                         col = pos[0] // CELL
                         row = pos[1] // CELL
                         if row < 0 or row >= m or col < 0 or col >= n:
                             dragging = False
                             continue
-                        if maze[row][col] == 0:
-                           maze[selectedCell[0]][selectedCell[1]] = 0  # 이전 위치를 비움
-                           maze[row][col] = 0  # 새 위치에 셀 배치
-                           if selectedCell == start:  # 시작점을 이동
+                        if grid[row][col] == 0:
+                           grid[focusCell[0]][focusCell[1]] = 0  # 이전 위치를 비움
+                           grid[row][col] = 0  # 새 위치에 셀 배치 
+                           if focusCell == start:  # 시작점을 이동
                                start = (row, col)
-                           elif selectedCell == end:  # 종료점을 이동
+                           elif focusCell == end:  # 종료점을 이동
                                end = (row, col)
-                           selectedCell = (row, col)
+                           focusCell = (row, col)
 
 
         # 화면 업데이트
         screen.fill(WHITE)
 
-        draw_maze(screen, maze, start, end, path, visit)  # 미로 그리기
+        drawGrid(screen, grid, start, end, path, visit)  # 미로 그리기
         pygame.draw.rect(screen, BUTTON, startButton)  # 시작 버튼 그리기
         pygame.draw.rect(screen, BUTTON, randomButton)  # 시작 버튼 그리기
         pygame.draw.rect(screen, BUTTON, resetButton)  # 리셋 버튼 그리기
@@ -414,20 +409,19 @@ def main(m, n, r):
         #버튼에 텍스트 추가
         font = pygame.font.Font(None, 36)
         text = font.render("Start A* Search", True, WHITE)  # 흰색 텍스트 생성
-        text_rect = text.get_rect(center=startButton.center)
-        screen.blit(text, text_rect)
+        textRect = text.get_rect(center=startButton.center)
+        screen.blit(text, textRect)
         text = font.render("Random walls", True, WHITE)  # 흰색 텍스트 생성
-        text_rect = text.get_rect(center=randomButton.center)
-        screen.blit(text, text_rect)
+        textRect = text.get_rect(center=randomButton.center)
+        screen.blit(text, textRect)
         text = font.render("Reset", True, WHITE)  # 초기화 버튼 텍스트 생성
-        text_rect = text.get_rect(center=resetButton.center)
+        textRect = text.get_rect(center=resetButton.center)
         app.paint(screen)
-        screen.blit(text, text_rect)
+        screen.blit(text, textRect)
         pygame.display.flip()
 
     pygame.quit()
     sys.exit()
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='options: --m --n --r')
